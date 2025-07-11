f7f295447e8bf37f3957f0ec850c7b8c0e03929c
xuandav <164993335+xuandav@users.noreply.github.com>
2025-07-10 23:31:47 -0500

    Add .next/static/chunks/src_app_game_[id]_page_tsx_a993527f._.js 1752208307196

---
+++ .next/static/chunks/src_app_game_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_f67271e5._.js",
+  "static/chunks/src_6b889fbb._.js"
+],
+    source: "dynamic"
+});
