81c3699098787d216ab3516725520bf934b6f974
xuandav <164993335+xuandav@users.noreply.github.com>
2025-07-10 23:31:41 -0500

    Add .next/static/chunks/src_app_admin_page_tsx_a993527f._.js 1752208300937

---
+++ .next/static/chunks/src_app_admin_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b0d899de._.js",
+  "static/chunks/src_e68f06ce._.js"
+],
+    source: "dynamic"
+});
